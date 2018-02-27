a843e1e4de60198a5edb5721f7a26b5e1d50aaee
humingx <wenjie.zhang@aorise.org>
2018-02-27 18:07:45 +0800

    init

--- app/src/main/java/com/example/z1310_000/sharedppx/activity/LoginActivity.java
+++ app/src/main/java/com/example/z1310_000/sharedppx/activity/LoginActivity.java
@@ -3,6 +3,8 @@ package com.example.z1310_000.sharedppx.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.preference.Preference;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.z1310_000.sharedppx.R;
@@ -29,11 +32,23 @@ import org.litepal.tablemanager.Connector;
 import cz.msebera.android.httpclient.Header;
 import cz.msebera.android.httpclient.entity.StringEntity;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
 public class LoginActivity extends AppCompatActivity {
     private User nowUser;
     private EditText userEdit, pwdEdit;
     private Button loginbtn, registerbtn;
     private CheckBox autoLogin;
+    private ImageView href;
     private AsyncHttpClient client = new AsyncHttpClient();
 
     private LoginRequest loginRequest=new LoginRequest();
@@ -45,6 +60,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Log.e("SHA1", "onCreate: "+getCertificateSHA1Fingerprint(this) );
 
         initView();
 
@@ -67,6 +84,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void initView() {
+        href= ( ImageView ) findViewById(R.id.href);
         userEdit = (EditText) findViewById(R.id.username);
         pwdEdit = (EditText) findViewById(R.id.password);
         loginbtn = (Button) findViewById(R.id.loginbtn);
@@ -97,6 +115,12 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
+            }
+        });
+        href.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TestActivity.startAction(getApplicationContext());
             }
         });
     }
@@ -144,5 +168,71 @@ public class LoginActivity extends AppCompatActivity {
     public static void startAction(Context context){
         Intent intent=new Intent(context,LoginActivity.class);
         context.startActivity(intent);
+    }
+    //这个是获取SHA1的方法
+    public static String getCertificateSHA1Fingerprint(Context context) {
+        //获取包管理器
+        PackageManager pm = context.getPackageManager();
+        //获取当前要获取SHA1值的包名，也可以用其他的包名，但需要注意，
+        //在用其他包名的前提是，此方法传递的参数Context应该是对应包的上下文。
+        String packageName = context.getPackageName();
+        //返回包括在包中的签名信息
+        int flags = PackageManager.GET_SIGNATURES;
+        PackageInfo packageInfo = null;
+        try {
+            //获得包的所有内容信息类
+            packageInfo = pm.getPackageInfo(packageName, flags);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        //签名信息
+        Signature[] signatures = packageInfo.signatures;
+        byte[] cert = signatures[0].toByteArray();
+        //将签名转换为字节数组流
+        InputStream input = new ByteArrayInputStream(cert);
+        //证书工厂类，这个类实现了出厂合格证算法的功能
+        CertificateFactory cf = null;
+        try {
+            cf = CertificateFactory.getInstance("X509");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //X509证书，X.509是一种非常通用的证书格式
+        X509Certificate c = null;
+        try {
+            c = (X509Certificate) cf.generateCertificate(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String hexString = null;
+        try {
+            //加密算法的类，这里的参数可以使MD4,MD5等加密算法
+            MessageDigest md = MessageDigest.getInstance("SHA1");
+            //获得公钥
+            byte[] publicKey = md.digest(c.getEncoded());
+            //字节到十六进制的格式转换
+            hexString = byte2HexFormatted(publicKey);
+        } catch (NoSuchAlgorithmException e1) {
+            e1.printStackTrace();
+        } catch (CertificateEncodingException e) {
+            e.printStackTrace();
+        }
+        return hexString;
+    }
+    //这里是将获取到得编码进行16进制转换
+    private static String byte2HexFormatted(byte[] arr) {
+        StringBuilder str = new StringBuilder(arr.length * 2);
+        for (int i = 0; i < arr.length; i++) {
+            String h = Integer.toHexString(arr[i]);
+            int l = h.length();
+            if (l == 1)
+                h = "0" + h;
+            if (l > 2)
+                h = h.substring(l - 2, l);
+            str.append(h.toUpperCase());
+            if (i < (arr.length - 1))
+                str.append(':');
+        }
+        return str.toString();
     }
 }
---
+++ app/src/main/java/com/example/z1310_000/sharedppx/activity/TestActivity.java
@@ -0,0 +1,83 @@
+package com.example.z1310_000.sharedppx.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.z1310_000.sharedppx.R;
+import com.example.z1310_000.sharedppx.request.UserService;
+import com.example.z1310_000.sharedppx.utils.AppConfig;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+public class TestActivity extends AppCompatActivity {
+    private static final String TAG = "TestActivity";
+    private TextView textView;
+    private EditText text_username,text_password;
+    private Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+        initView();
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initRetrofit();
+            }
+        });
+    }
+
+    private void  initView(){
+        text_username= ( EditText ) findViewById(R.id.text_username);
+        text_password= ( EditText ) findViewById(R.id.text_password);
+        loginBtn= ( Button ) findViewById(R.id.loginBtn);
+        textView= ( TextView ) findViewById(R.id.textView);
+    }
+
+    private void initRetrofit(){
+        Log.e(TAG, "initRetrofit: " );
+        //获取Retrofit对象，设置地址
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(AppConfig.URL_BASE)
+                .build();
+        UserService userService=retrofit.create(UserService.class);
+        Call<ResponseBody> call = userService.userLogin("qqq","123");
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                Log.e(TAG, "success: "+call);
+                try {
+                    textView.setText(response.body().string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Toast.makeText(TestActivity.this, response.body().toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Log.e(TAG, "onFailure: " );
+            }
+        });
+    }
+
+    public static void startAction(Context context){
+        Intent intent=new Intent(context,TestActivity.class);
+        context.startActivity(intent);
+    }
+}
