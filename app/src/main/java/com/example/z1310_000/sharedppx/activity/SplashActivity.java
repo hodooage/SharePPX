07084d05a1b30567e3902213d208c5a93e97e7b2
humingx <wenjie.zhang@aorise.org>
2018-06-13 16:07:58 +0800

    倒计时添加progressBar提示

--- app/src/main/java/com/example/z1310_000/sharedppx/activity/SplashActivity.java
+++ app/src/main/java/com/example/z1310_000/sharedppx/activity/SplashActivity.java
@@ -1,15 +1,18 @@
 package com.example.z1310_000.sharedppx.activity;
 
+import android.annotation.TargetApi;
 import android.app.WallpaperManager;
 import android.databinding.DataBindingUtil;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -50,9 +53,12 @@ public class SplashActivity extends AppCompatActivity {
     private void initSplash(){
         //倒计时开始
         CountDownTimer countDownTimer=new CountDownTimer(5000,1000) {
+            @TargetApi(Build.VERSION_CODES.M)
             @Override
             public void onTick(long l) {
-                    mBinding.countDown.setText(String.valueOf(l/1000));
+                mBinding.progressBar.setMax(5);
+                mBinding.progressBar.setProgress(5-(int)l/1000);
+                mBinding.countDown.setText(String.valueOf(l/1000));
             }
 
             @Override
@@ -69,7 +75,7 @@ public class SplashActivity extends AppCompatActivity {
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
                 try {
                     //txt.setText(response.body().string());
-
+                    //这里有个坑，response.body().string()只能调用一次，第二次调用会报空指针异常
                     WallPaper wallPaper=new Gson().fromJson(response.body().string(),WallPaper.class);
 
                     String url=wallPaper.getRes().getVertical().get(0).getWp();
